8124725aaeda2752ec8d64d97f828b41ae7bf300
John <wss.office21@gmail.com>
2023-02-06 22:44:04 +0200

    ver. 1.2.3

--- inc/front/layouts/sort/sort-posts.php
+++ inc/front/layouts/sort/sort-posts.php
@@ -9,7 +9,7 @@ echo '<div class="menu-passive">';
 echo '<i class="btn-close">x</i>';
 $criteria_sort_post =  '<div class="menu-passive__item"><a class="menu-link" data-order="'.esc_attr('desc').'" data-orderby="'.esc_attr('title').'" href="#">'.esc_html__('Sort by title', 'ymc-smart-filter').'</a></div>';
 $criteria_sort_post .= '<div class="menu-passive__item"><a class="menu-link" data-order="'.esc_attr('desc').'" data-orderby="'.esc_attr('date').'" href="#">'.esc_html__('Sort by publication date', 'ymc-smart-filter').'</a></div>';
-echo $default_order_by = apply_filters('ymc_sort_posts_by_'.$c_target, $criteria_sort_post);
+echo apply_filters('ymc_sort_posts_by_'.$c_target, $criteria_sort_post);
 echo '</div>';
 echo '</div>';
 echo '</div>';
