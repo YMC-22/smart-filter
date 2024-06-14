429f321a9791860751dd6f7ebc010ddbca1306d7
DAnn2012 <dannunziata@hotmail.it>
2024-06-14 10:55:37 +0200

    Update advanced.php

--- includes/core/admin/tabs/advanced.php
+++ includes/core/admin/tabs/advanced.php
@@ -236,10 +236,10 @@
                         $selected_preloader = 'selected="selected"';
                     }
                     if( $i === 11 ) {
-                        echo '<option value="preloader_'.$i.'" '. $selected_preloader .'>'. esc_html('None', 'ymc-smart-filter') .'</option>';
+                        echo '<option value="preloader_'.$i.'" '. $selected_preloader .'>'. esc_html__('None', 'ymc-smart-filter') .'</option>';
                     }
                     else {
-                        echo '<option value="preloader_'.$i.'" '. $selected_preloader .'>'. esc_html('Preloader '.$i, 'ymc-smart-filter') .'</option>';
+                        echo '<option value="preloader_'.$i.'" '. $selected_preloader .'>'. esc_html__('Preloader ', 'ymc-smart-filter') .$i .'</option>';
                     }
                     $selected_preloader = '';
                 }
