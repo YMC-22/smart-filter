d12bc9a79b416269c8b38de0b498ec46327071c9
YMC <wss.office21@gmail.com>
2024-09-21 12:50:48 +0300

    update 2.9.36

--- includes/core/frontend/layouts/filter/filter-range.php
+++ includes/core/frontend/layouts/filter/filter-range.php
@@ -1,5 +1,28 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit;
 
+// Add Style
+$filter_css = "";
+if( !empty($ymc_filter_text_color) ) {
+	$filter_css .= "#ymc-smart-filter-container-" . $c_target . " .filter-range .filter-entry,
+	#ymc-extra-filter-" . $c_target . " .filter-range .filter-entry {color:" . $ymc_filter_text_color . "}";
+
+	$filter_css .= "#ymc-smart-filter-container-" . $c_target . " .filter-range .filter-entry .range__component,
+	#ymc-extra-filter-" . $c_target . " .filter-range .filter-entry .range__component {color:" . $ymc_filter_text_color . "}";
+}
+if( !empty($ymc_filter_bg_color) ) {
+	$filter_css .= "#ymc-smart-filter-container-".$c_target." .filter-range .filter-entry .range__component:not(.tax-label):not(.apply-button),	
+	#ymc-extra-filter-".$c_target." .filter-range .filter-entry .range__component:not(.tax-label):not(.apply-button) {background-color:".$ymc_filter_bg_color."}";
+
+	$filter_css .= "#ymc-smart-filter-container-".$c_target." .filter-range .filter-entry .tag-values:before,	
+	#ymc-extra-filter-".$c_target." .filter-range .filter-entry .tag-values:before {border-top: 12px solid ".$ymc_filter_bg_color."}";
+}
+if( $ymc_filter_font !== 'inherit' ) {
+	$filter_css .= "#ymc-smart-filter-container-".$c_target." .filter-range .filter-entry,               
+    #ymc-extra-filter-".$c_target." .filter-range .filter-entry {font-family:".$ymc_filter_font."}";
+}
+
+echo '<style id="'.$handle_filter.'">'. preg_replace('|\s+|', ' ', $filter_css) .'</style>';
+
 ?>
 
 <div id="<?php echo esc_attr($ymc_filter_layout); ?>-<?php echo esc_attr($c_target); ?>" class="filter-layout <?php echo esc_attr($ymc_filter_layout); ?> <?php echo esc_attr($ymc_filter_layout); ?>-<?php echo esc_attr($id); ?> <?php echo esc_attr($ymc_filter_layout); ?>-<?php echo esc_attr($id); ?>-<?php echo esc_attr($c_target); ?>">
